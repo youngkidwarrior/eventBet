9096e12a8e866989a574338566b37c4893a03580
YoungKidWarrior <vginelli18@cmc.edu>
2019-01-11 23:15:54 -0800

    Bookings!
    Updated GraphQL Schema:
         booking mongoose model
         create and cancel bookings
    New singleEvent function allows bookings to be infinitely tied to events

--- backend/graphql/schema/index.js
+++ backend/graphql/schema/index.js
@@ -6,22 +6,28 @@ const { buildSchema } = require('graphql');
 ///     - ! is required, notice password is not required                ///
 ///        for the query because password should never be returned      ///
 ///     - Layout                                                        ///
-///         User -> Events                                               ///
+///         User -> one to many -> Events                               ///
 ///////////////////////////////////////////////////////////////////////////
 module.exports = buildSchema(`
-    
         input EventInput {
-        title: String! 
-        description: String!
-        price: Float!
-        date: String!
+            title: String! 
+            description: String!
+            price: Float!
+            date: String!
         }
 
         input UserInput {
-        email: String!
-        password: String!
+            email: String!
+            password: String!
         }
-        
+
+        type Booking {
+            _id: ID!
+            event: Event!
+            user: User!
+            createdAt: String!
+            updateAt: String!
+        }
         type Event {
             _id: ID!
             title: String!
@@ -32,25 +38,27 @@ module.exports = buildSchema(`
         }
 
         type User {
-        _id: ID!
-        email: String!
-        password: String
-        createdEvents: [Event!]
+            _id: ID!
+            email: String!
+            password: String
+            createdEvents: [Event!]
         }
 
         type rootQuery {
-        events: [Event!]!
-        users: [User!]!
+            events: [Event!]!
+            users: [User!]!
+            bookings: [Booking!]!
         }
 
         type rootMutation {
-        createEvent(eventInput: EventInput): Event 
-        createUser(userInput: UserInput): User
+            createEvent(eventInput: EventInput): Event 
+            createUser(userInput: UserInput): User
+            bookEvent(eventId: ID!): Booking!
+            cancelBooking(bookingId: ID!): Event!
         }
 
         schema {
-        query: rootQuery
-        mutation: rootMutation
+            query: rootQuery
+            mutation: rootMutation
         }
-        `
-        );
+    `);
